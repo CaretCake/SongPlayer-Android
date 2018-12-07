5a7ea58800df5587b0bedb50a9a5ad4920ef7719
Melissa Farrell <melissa.farrell@ibm.com>
2018-12-07 13:06:40 -0500

    SongLab populates list with songs from website

--- app/src/main/java/c/melissa/songplayer/SongLab.kt
+++ app/src/main/java/c/melissa/songplayer/SongLab.kt
@@ -8,12 +8,23 @@
 package c.melissa.songplayer
 
 import android.content.Context
+import android.os.AsyncTask
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.AccessController.getContext
 
 import java.util.ArrayList
 import java.util.UUID
 
 class SongLab private constructor(context: Context) {
     private val mSongs: MutableList<Song>
+    internal var webthread: Boolean = false
+    internal var htmlText: String = ""
+    var urlSongMap:MutableMap<String, String> = mutableMapOf<String, String>()
 
     val songs: List<Song>
         get() = mSongs
@@ -29,10 +40,21 @@ class SongLab private constructor(context: Context) {
     init { //Constructor is private; so no outside force can make a new one
         mSongs = ArrayList()
         val res = context.resources
+
+        webthread = false
+        RetrieveFeedTask().execute(null as Void?)
+        while (!webthread);
+        println("Wow!  What a great website!")
+
+        parseUrls()
+
+
         // TODO: change to get list from website content
 
-        for (i in 0..29) {
+        for (song in urlSongMap.keys) {
             val newSong = Song()
+            newSong.songName = song
+            newSong.songLink = urlSongMap.get(song)
             mSongs.add(newSong)
         }
     }
@@ -44,6 +66,72 @@ class SongLab private constructor(context: Context) {
         operator fun get(context: Context): SongLab {
             if (sSongLab == null) sSongLab = SongLab(context)
             return sSongLab as SongLab
+        }
+    }
+
+
+    fun parseUrls () {
+        val regexPattern = Regex("<A HREF=\"(\\/(\\d|\\w|\\/|%)*)\\..{3}\">((\\s|\\d|\\w)*)\\.(.{3})<\\/A>")
+        val foundMatches = regexPattern.findAll(htmlText)
+
+        foundMatches.forEach { match ->
+            /*println(match)
+            println(match.groups[0]) // Full match
+            println(match.groups[1]) // URL
+            println(match.groups[3]) // Name
+            println(match.groups[5]) // File type*/
+            if (!urlSongMap.containsKey(match.groups[3]!!.value)) {
+                urlSongMap[match.groups[3]!!.value] = match.groups[1]!!.value
+                //println("map at: " + match.groups[3]!!.value + " is now: " + match.groups[1]!!.value)
+            }
+        }
+    }
+
+    internal inner class RetrieveFeedTask : AsyncTask<Void, Void, Void>() {
+
+        private val exception: Exception? = null
+
+        override fun doInBackground(vararg urls: Void): Void? {
+            val context = getContext() as Context
+            println("About to begin")
+            try {
+                val obj = URL("http://philos.nmu.edu/weirdal/")
+                val con = obj.openConnection() as HttpURLConnection
+                println("Connected")
+                con.requestMethod = "GET"
+                val responseCode = con.responseCode
+                println("GET Response Code :: $responseCode")
+                if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                    var `in` = BufferedReader(InputStreamReader(con.inputStream))
+                    val tmpdir = context.cacheDir.toString()
+                    val out = PrintWriter("$tmpdir/license.txt")
+                    while (true) {
+                        val c = `in`.read()
+                        if (c == -1) break
+                        out.print(c.toChar())
+                    }
+                    `in`.close()
+
+                    out.close()
+                    `in` = BufferedReader(InputStreamReader(FileInputStream("$tmpdir/license.txt")))
+                    while (true) {
+                        val c = `in`.read()
+                        if (c == -1) break
+                        print(c.toChar())
+                        htmlText += c.toChar()
+                    }
+                    `in`.close()
+
+                } else {
+                    println("GET request not worked")
+                }
+            } catch (e: Exception) {
+                println("Yeah")
+                e.printStackTrace()
+            }
+
+            webthread = true
+            return null
         }
     }
 }
